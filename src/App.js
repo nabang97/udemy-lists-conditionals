09d050c64002843bdfd430acd1410f9a43a43f88
nabang97 <parawitayolanda@gmail.com>
2020-03-12 17:02:47 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userInput : ''
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState({userInput:event.target.value});
+  }
+
+  deleteCharHandler = (index) =>{
+    const text = this.state.userInput.split('');
+    text.splice(index,1);
+
+    const updatedText = text.join('');
+    this.setState({userInput:updatedText});
+  }
+
+  render(){
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char key ={index} character = {ch} clicked ={()=>this.deleteCharHandler(index)}/>
+    })
+
+    return (
+      <div className="App">
+        <input type="text" onChange={this.inputChangedHandler} value={this.state.userInput}></input>
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length}/>
+        {charList}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Validation/Validation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const validation = (props) =>{
+    let validationMessage = 'Text long enough!';
+
+    if (props.inputLength < 5 ){
+        validationMessage = 'Text too short'
+    }
+    return <p>{validationMessage}</p>
+}
+
+export default validation;
